dcaa8fc112401006e4786ed945b2c9c8485595ef
Valerii Bihun <valeriibihun.co@gmail.com>
2022-02-11 12:20:07 -0700

    Portfolio section refactored

---
+++ my-app/src/components/pages/Project.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import './styles.css';
+
+
+const Project = ({title,tech,description,deployedApp,gitHub,img}) => {
+    return (
+        <div className="project">
+<                   div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div className="flex-grow-1">
+                            <h3 className="mb-0">{title}</h3>
+                            <div className="subheading mb-3">{tech}</div>
+                            <p>{description}</p>
+                        </div>
+                        <div className="flex-shrink-0"><span className="text-primary"> <a href={deployedApp} target="_blank">Deployed application </a></span></div>
+                        <div className="flex-shrink-0"><span className="text-primary"> | <a href={gitHub} target="_blank"> GitHub Repo</a></span></div>
+                    </div>
+        </div>
+    )
+};
+
+export default Project
---
+++ my-app/src/components/pages/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+    {
+        id: 1,
+        title: "WEATHER DASHBOARD",
+        tech: "API, JAVASCRIPT, HTML, CSS.",
+        description:"This is a a simple weather application that allows a user to find out the weather forecast in the city! This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.",
+        deployedApp: "https://valllerian.github.io/Weather-Dashboard/",
+        gitHub: "https://github.com/Valllerian/Weather-Dashboard", 
+        img: "https://media.gettyimages.com/photos/pikes-peak-in-denver-cityscape-picture-id1179434642?s=612x612",
+    },
+    
+];
